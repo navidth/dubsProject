d7c0ced02d1e5566b1f599c1bfab18f9949bde7e
navid <navidtaheri32@gmail.com>
2023-12-29 18:52:46 +0330

    update detail items

--- src/components/DetailProduct.jsx
+++ src/components/DetailProduct.jsx
@@ -26,13 +26,21 @@ const DetailProduct = () => {
          setShowMassege(false)
         },2000)
     }
+    const[readMe,setReadMe] = useState (false);
+    const readMeBtn = () => {
+        setReadMe(!readMe)
+    }
+    const[changeItems,setChangeItems] = useState(true);
+    const changeItemsFunctions = () =>{
+        setChangeItems(!changeItems)
+    }
     return (
         <div className="">
             <div className='detailed-items my-5'>
             <div className="information-product bg-white rounded-4">
                 {/* modal img */}
-                <div className="img-products mt-4">
-                    <img src={pro} alt="product" className='me-3' width={'80%'} height={'80%'}/>
+                <div className="img-products mt-lg-4">
+                    <img src={pro} alt="product" className='me-3'/>
                     <div className="modal-img my-4 me-3">
                     <ModalImage 
                     smallSrcSet={pro}
@@ -170,13 +178,13 @@ const DetailProduct = () => {
                         <div className="prices d-flex align-items-baseline text-danger mb-4 mt-2">
                             <h1 className='px-2 letter-spacing ms-2'>{numberWithCommas(9180000)}</h1> <h6 className='letter-spacing'>تومان </h6>
                         </div>
-                        <div className="button-price d-flex align-items-center justify-content-between">
+                        <div className="button-price">
                             <div className="btn1">
                             <button type="button" className='btn btn-danger rounded-5 py-3 px-3 d-flex align-items-center'>
                                <MdAddShoppingCart size={'24px'}></MdAddShoppingCart > <span className='mx-3'>افزودن به سبد خرید</span> 
                             </button>
                             </div>
-                            <div className="btn2 mx-5">
+                            <div className="btn2 ">
                             <a target="_blank" rel="noopener noreferrer"  href="https://t.me/Navid_th?fbclid=PAAaYPv4EGttqdRmuE9EGoSA6yAUZ8VJkwX0OniF10f5Q0b2iqhQ768Mooc_k" className='btn btn-danger rounded-5 py-3 px-5 d-flex align-items-center'>
                                <BsTelegram size={'24px'}></BsTelegram > <span className='me-3'>مشاوره خرید </span> 
                             </a>
@@ -191,9 +199,38 @@ const DetailProduct = () => {
                     </section>
                 </div>
             </div>
-        <div className="description"></div>
-        <div className="similar-products"></div>
+            {/* description  and similar  product */}
+        <div className="description-similar bg-white rounded-4 my-5">
+            <section>
+                <div className="d-flex justify-content-around">
+                <a href='#one' onClick={changeItemsFunctions} className='px-3 py-2 w-50 fs-4' style={{ background: changeItems ? '#dc3545' : '' , color:changeItems ? '#f8f9fa ' : ''}}>توضیحات</a>
+                <a  href="#two" onClick={changeItemsFunctions} className='px-3 py-2 w-50 fs-4' style={{ background: changeItems ? '' : '#dc3545' , color:changeItems ? ' ' : '#f8f9fa'}}>محصولات مشابه</a >
+                </div>
+            </section>
+            {/* // desceriptions...........................  */}
+            <div id='one' className="desceriptions">
+           <section className="header-desceription px-3 py-2 mt-3">
+               <h2> معرفی چوب راکت ویسکاریا (Viscaria) </h2>
+            </section>
+            <section>
+                <h4 className='px-4 mx-3 fw-700 letter-spacing'> معرفی کوتاه</h4>
+                {readMe && 
+                <div>
+                   <p id='text' className='p-desceriptions'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                                    </p>
+                </div>
+                }
+                <button type='button' onClick={readMeBtn} className='fs-4 btn btn-readMe btn-danger text-white w-100 mx-auto d-block'>
+                 {readMe ? "مخفی کردن" : "نمایش کامل"}
+                </button>
+            </section>
+           </div> 
+          {/* //    similar product............................... */}
+           <div id='two' className="similar-products">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae tempora, molestiae excepturi sint nobis exercitationem magnam deleniti nemo consectetur aut architecto cum? Velit inventore atque, itaque, consectetur maxime fugit facilis vitae ipsa pariatur sequi laudantium voluptatum cupiditate laboriosam enim aspernatur?</p>
+           </div>
         </div>
+     </div>
 
             <Tooltip
         id="my-tooltip"
