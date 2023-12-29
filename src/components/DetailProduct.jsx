ad17da7aab7874cd6d2c428288ecb35925038174
navid <navidtaheri32@gmail.com>
2023-12-29 12:36:30 +0330

    update pages

--- src/components/DetailProduct.jsx
+++ src/components/DetailProduct.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Header from './Header';
 import pro from '../assest/images/image2.jpg';
 import { BsBookmark,BsBookmarkFill,BsShare,BsTelegram} from "react-icons/bs";
 import { MdAddShoppingCart } from "react-icons/md";
 import { Tooltip } from 'react-tooltip';
 import ModalImage from "react-modal-image";
-import { ItemMenu } from './Data';
 
 const DetailProduct = () => {
     const [bookMarks,setBookmarks] = useState(false);
@@ -19,6 +16,9 @@ const DetailProduct = () => {
 
         },1000)
     }
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
     const handleDashbordFavorite = ()=>{
         setBookmarks(!bookMarks);
         setShowMassege(true)
@@ -29,11 +29,8 @@ const DetailProduct = () => {
     return (
         <div className="">
             <div className='detailed-items my-5'>
-            <div className="items">
-            <div className="address d-inline-block my-3">
-                <Header pageName=' چوب راکت باترفلای' pageFather={ItemMenu[0]}></Header>
-            </div>
             <div className="information-product bg-white rounded-4">
+                {/* modal img */}
                 <div className="img-products mt-4">
                     <img src={pro} alt="product" className='me-3' width={'80%'} height={'80%'}/>
                     <div className="modal-img my-4 me-3">
@@ -61,6 +58,7 @@ const DetailProduct = () => {
                     />
                     </div>
                 </div>
+                {/* body information product */}
                 <div className="body-product mt-3">
                     <section> 
                         <div className="d-flex justify-content-between align-items-center">
@@ -168,20 +166,24 @@ const DetailProduct = () => {
                         </li>
                         </ul> 
                     </section>
-                    <section>
-                        <div className="prices d-flex align-items-baseline text-danger mb-3">
-                            <h2 className='px-2 letter-spacing'>9180000</h2> <h6 className='letter-spacing'>تومان </h6>
+                    <section className='pb-4'>
+                        <div className="prices d-flex align-items-baseline text-danger mb-4 mt-2">
+                            <h1 className='px-2 letter-spacing ms-2'>{numberWithCommas(9180000)}</h1> <h6 className='letter-spacing'>تومان </h6>
                         </div>
                         <div className="button-price d-flex align-items-center justify-content-between">
+                            <div className="btn1">
                             <button type="button" className='btn btn-danger rounded-5 py-3 px-3 d-flex align-items-center'>
                                <MdAddShoppingCart size={'24px'}></MdAddShoppingCart > <span className='mx-3'>افزودن به سبد خرید</span> 
                             </button>
+                            </div>
+                            <div className="btn2 mx-5">
                             <a target="_blank" rel="noopener noreferrer"  href="https://t.me/Navid_th?fbclid=PAAaYPv4EGttqdRmuE9EGoSA6yAUZ8VJkwX0OniF10f5Q0b2iqhQ768Mooc_k" className='btn btn-danger rounded-5 py-3 px-5 d-flex align-items-center'>
                                <BsTelegram size={'24px'}></BsTelegram > <span className='me-3'>مشاوره خرید </span> 
                             </a>
+                            </div>
                         </div>
                     </section>
-                    <section className='mt-2 mb-3 border-0'>
+                    <section className='my-1 border-0'>
                         <div className="stock">
                             <label htmlFor="stock" className='fw-bold fs-small'>موجودی : </label>
                             <span className='text-danger'> موجود</span>
@@ -189,11 +191,9 @@ const DetailProduct = () => {
                     </section>
                 </div>
             </div>
-            </div>
-
-        </div>
         <div className="description"></div>
         <div className="similar-products"></div>
+        </div>
 
             <Tooltip
         id="my-tooltip"
--- src/components/cardProduct.jsx
+++ src/components/cardProduct.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const CardProduct = ({Item}) => {
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 return(
 <React.Fragment>
     {Item.map((post, index) => {
     if (index % 2 === 0) {
-      const formattedPrice = post.price.toLocaleString("fa-IR");
+      // const formattedPrice = post.price.toLocaleString("fa-IR");
       return (
         <div
           className="collectionProduct mt-4"
@@ -21,7 +24,7 @@ return(
                 <p className="nameProduct h4 w3-opacity-min ">{post.name}</p>
               </div>
               <div className="PricePRO">
-                <span className="fw-bolder fs-5 ms-2">{formattedPrice}</span>{" "}
+                <span className="fw-bolder fs-5 ms-2 letter-spacing ">{numberWithCommas(post.price)}</span>{"  "}
                 <span className="opacity-75">تومان</span>
               </div>
             </div>
@@ -45,8 +48,8 @@ return(
                   </p>
                 </div>
                 <div className="PricePRO">
-                  <span className="fw-bolder fs-5 ms-2">
-                    {Item[index + 1].price.toLocaleString('fa-IR')}
+                  <span className="fw-bolder fs-5 ms-2 letter-spacing ">
+                    {numberWithCommas(Item[index + 1].price)}
                   </span>{" "}
                   <span className="opacity-75">تومان</span>
                 </div>
