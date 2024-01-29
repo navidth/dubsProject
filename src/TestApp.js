ece2c20928207cbffd2e4e1d9166046b5c0326f7
navid <navidtaheri32@gmail.com>
2024-01-29 12:49:43 +0330

    Updates

--- src/TestApp.js
+++
@@ -1,5 +0,0 @@
-import Test from "./components/test";
-const TestApp=()=>{
-
-}
-export default TestApp;
--- src/pages/login and buy/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../../assest/css/register.css';
-import Register from '../../components/register';
-const Login =()=>{
-	return(
-		<Register></Register>
-	)
-}
-export default Login;
